2cf348b929e64e1cf396838305c402f3f5800624
Peter12-s <peterestrada01.millan@gmail.com>
2025-09-05 22:32:41 -0600

    añadiendo logout  y pintar nombre de usuario

--- administrador/inicioAdmin.js
+++ administrador/inicioAdmin.js
@@ -2,7 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger");
     const menu = document.getElementById("menu");
     const menuItems = document.querySelectorAll(".menu-item > a");
-
+   //obtener el nombre del usuario del localStorage
+    const userName = localStorage.getItem("user_name");
+    //mostrar el nombre del usuario en el elemento con id userName
+    
+    if (userName) {
+        document.getElementById("user").textContent = userName;
+    }
     // Menú principal en móvil
     hamburger.addEventListener("click", () => {
         menu.classList.toggle("show");
