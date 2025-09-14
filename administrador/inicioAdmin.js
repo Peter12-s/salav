a2ab229ffb3d0192e6f229fc92b3c925c845bde8
Peter12-s <peterestrada01.millan@gmail.com>
2025-09-13 23:33:03 -0600

    nombreCOmpany

--- administrador/inicioAdmin.js
+++ administrador/inicioAdmin.js
@@ -1,26 +1,2 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.getElementById("hamburger");
-    const menu = document.getElementById("menu");
-    const menuItems = document.querySelectorAll(".menu-item > a");
-   //obtener el nombre del usuario del localStorage
-    const userName = localStorage.getItem("user_name");
-    //mostrar el nombre del usuario en el elemento con id userName
-    
-    if (userName) {
-        document.getElementById("user").textContent = userName;
-    }
-    // Menú principal en móvil
-    hamburger.addEventListener("click", () => {
-        menu.classList.toggle("show");
-    });
-
-    // Submenús
-    menuItems.forEach(item => {
-        item.addEventListener("click", (e) => {
-            if (window.innerWidth <= 768) { // móvil
-                e.preventDefault();
-                item.parentElement.classList.toggle("active");
-            }
-        });
-    });
-});
+const user=JSON.parse(localStorage.getItem('company-name'));
+document.getElementById('user').innerText=user;
