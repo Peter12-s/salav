d43c1a927e29cff086f3d15ddc48bd048f34c3f2
Peter12-s <peterestrada01.millan@gmail.com>
2025-09-13 23:41:41 -0600

    companyname

--- empresas/inicioEmpresa.js
+++ empresas/inicioEmpresa.js
@@ -1,3 +1,2 @@
-// //obtenemos el usuario de localStorage
-// const user =getLocalStorageItem('user_name') ? getLocalStorageItem('user_name') : '';
-// document.getElementById('user').textContent = user;
+const user=localStorage.getItem('company_name');
+document.getElementById('user').innerText=user;
